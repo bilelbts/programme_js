9becd29ac067e7445aa5e8d9da67173b3fb215b2
Bilal Hadjersi <150812204+bilelbts@users.noreply.github.com>
2025-07-18 06:14:56 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+lancerJeu()
+
+
+
+
+
+
+
+
+
+
+
+//console.log(divinputEcriture) 
+
+
+//let btnValiderMot = document.getElementById("btnValiderMot")
+//console.log(btnValiderMot)
+//let Proposition = document.querySelector(".zoneProposition")
+//console.log(Proposition)
+//let spanScore = document.querySelector(".zoneScore span")
+//console.log(spanScoreScore)
+
+//let listinputradio = document.querySelectorAll(".zoneOptions input")//
+//console.log(listinputradio)
